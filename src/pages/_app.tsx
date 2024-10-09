257e529daf56bee606fc469d0972df076bcb51fd
Raon98 <cheol1998@kakao.com>
2024-10-09 16:41:04 +0900

    feat : 10.9 2.3 navigating

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,25 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const onClickBtn = () => {
+    router.push("/test");
+  };
   return (
     <>
-      <div className="">글로벌 헤더</div>
+      <div className="">
+        <Link href={"/"}>index</Link>
+        &nbsp;
+        <Link href={"/search"}>search</Link>
+        &nbsp;
+        <Link href={"/book/1"}>book/1</Link>
+        <div>
+          <button onClick={() => onClickBtn()}>/test 패이지로 이동</button>
+        </div>
+      </div>
       <Component {...pageProps} />
     </>
   );
